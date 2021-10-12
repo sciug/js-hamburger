bf403eb1038e959b535ae2370cc53afe1437dce1
sciug <gaetano.sciurti@libero.it>
2021-10-12 14:06:41 +0200

    add closemenu event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
 var openMenu = document.querySelector(".fas.fa-bars")
-console.log(openMenu)
+var closeMenu = document.querySelector(".close")
 var hamMenu = document.querySelector(".hamburger-menu")
+
+
 
 openMenu.addEventListener("click", function() {
 
     hamMenu.classList.add("active")
 })
+
+closeMenu.addEventListener("click", function() {
+    hamMenu.classList.remove("active")
+    
+} )
